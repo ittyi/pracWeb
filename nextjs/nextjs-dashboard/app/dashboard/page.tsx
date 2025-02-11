16642adc7042e2366b99036fddba0bdaa41c93fd
ittyi <vioiv.k4@gmail.com>
2025-02-11 23:28:39 +0900

    fix: 模範解答の書き方にした。分割代入、使おうって脳みそに中々切り替わらないなー

--- nextjs/nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs/nextjs-dashboard/app/dashboard/page.tsx
@@ -6,14 +6,13 @@ import { fetchCardData, fetchRevenue, fetchLatestInvoices } from '@/app/lib/data
 
 export default async function Page() {
     const revenue = await fetchRevenue();
-    
     const latestInvoices = await fetchLatestInvoices();
-
-    const cardData = await fetchCardData();
-    const totalPaidInvoices = cardData.totalPaidInvoices;
-    const totalPendingInvoices = cardData.totalPendingInvoices;
-    const numberOfInvoices = cardData.numberOfInvoices;
-    const numberOfCustomers = cardData.numberOfCustomers;
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+    } = await fetchCardData();
 
     return (
         <main>
