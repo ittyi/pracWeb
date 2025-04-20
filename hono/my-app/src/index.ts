85e45fc2bfc101a94455b0cbf3e8861ebae3ec68
ittyi <vioiv.k4@gmail.com>
2025-04-20 23:39:45 +0900

    hono でパスパラメータを取得してみた

--- hono/my-app/src/index.ts
+++ hono/my-app/src/index.ts
@@ -13,4 +13,17 @@ app.get("/api/hello", (c) => {
   });
 });
 
+app.get("/posts/:id", (c) => {
+  // クエリパラメーターpageを取得
+  const page = c.req.query("page");
+
+  // パスパラメーターidを取得
+  const id = c.req.param("id");
+
+  // ヘッダーを設定
+  c.header("X-Message", "Hi!");
+
+  return c.text(`You want see ${page} of ${id}`);
+});
+
 export default app;
